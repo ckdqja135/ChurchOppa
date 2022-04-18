3b5f44962884130f15a8c8daf11cf0d6951bd56c
cbsong <ckdqja135@naver.com>
2022-04-18 15:47:17 +0900

    church Page URL 추가.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/assets/html/index.html');
 })
+
+app.get('/church', function(req, res){
+    res.sendFile(__dirname + '/assets/html/churchpage.html');
+})
+
 app.use('/', express.static(__dirname + '/assets'));
 // app.use(express.static('images'));
 app.listen(3000, () => console.log('3000번 포트 대기'));
