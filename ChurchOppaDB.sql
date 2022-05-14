3e8334f1b9a4add8589b745ce1280423e27b077a
송창범 <cbsong@mysoftwiz.com>
2022-05-14 12:47:29 +0900

    SQL문 추가.

--- ChurchOppaDB.sql
+++ ChurchOppaDB.sql
@@ -15,12 +15,17 @@
 --   PRIMARY KEY (`ChurchNo`))
 -- ENGINE = InnoDB
 
--- select * from churchinfo
+-- 교회 데이터 insert
+-- INSERT INTO `churchoppa`.`churchinfo` 
+-- (`ChurchNo`, `ChurchName`, `ChurchLocate`, `ChurchReli`, `ChurchScales`, `ChurchPastor`, `ChurchTel`, `ChurchLateX`, `ChurchLateY`, `ChurchHome`, `ChurchSerIMG`, `ChurchJibun`, `ChurchAddr`, `ChurchMapIMG`) VALUES 
+-- ('0', '새지음교회', '서울시 강동구', '기하성', '중', '박지민', '02-483-3277', '37.549219', '127.128118', 'https://www.ncchurch.kr/', 'https://user-images.githubusercontent.com/33046341/165232296-83913b29-d259-45f5-9873-2e886db625c2.png', '(우)12922 경기도 하남시 미사강변북로 122', '경기도 하남시 망월동 122-22', 'https://yt3.ggpht.com/ytc/AKedOLRzfoBtzKKqM2YLjjIOufODb25UrJnxYEHLNyfC=s900-c-k-c0x00ffffff-no-rj');
+
+select * from churchinfo
 -- 칼럼 값 수정 시 사용.
 -- ALTER TABLE ChurchInfo MODIFY ChurchLateX DOUBLE
 -- ALTER TABLE ChurchInfo MODIFY ChurchLateY DOUBLE
 
 --  칼럼 추가
--- ALTER TABLE ChurchInfo ADD ChurchAddr VARCHAR(200) NULL
+-- ALTER TABLE ChurchInfo ADD ChurchMapIMG VARCHAR(200) NULL
 -- 칼럼명 변경
 -- ALTER TABLE ChurchInfo CHANGE ChurchLPost ChurchAddr VARCHAR(200) NULL--
